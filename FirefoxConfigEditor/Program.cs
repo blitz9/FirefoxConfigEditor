80d33426ed6b302cff06c788f319860ce087aa5c
Blitz9 <morozov.vladik2011@yandex.ru>
2019-02-17 17:58:11 +1000

    Add change pref.js

--- FirefoxConfigEditor/Program.cs
+++ FirefoxConfigEditor/Program.cs
@@ -3,13 +3,173 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FirefoxConfigEditor
 {
+    sealed class Parameter
+    {
+        public enum ParamType
+        {
+            delete,
+            add,
+            unknow
+        }
+        Parameter() { }
+
+        public Parameter(string name, string value, ParamType type)
+        {
+            Name = name;
+            Value = value;
+            ParameterType = type;
+            Name = name;
+            Value = value;
+            ParameterType = type;
+        }
+
+        private string name;
+        private string value;
+        private ParamType type;
+
+        public string Name
+        { get => name; set => name = value; }
+        public string Value { get => value; set => this.value = value; }
+        public ParamType ParameterType { get => type; set => type = value; }
+
+        public string ParamToString()
+        {
+            return $"user_pref({Name},{Value});";
+        }
+
+        public static implicit operator Parameter(string param)
+        {
+            ParamType type;
+
+            switch (param[0])
+            {
+                case '+':
+                    type = ParamType.add;
+                    break;
+                case '-':
+                    type = ParamType.delete;
+                    break;
+                default:
+                    type = ParamType.unknow;
+                    break;
+            }
+
+            string name = param.Substring(param.IndexOf('(') + 1, param.IndexOf(',') - param.IndexOf('(') - 1);
+            string value = param.Substring(param.IndexOf(',') + 1, param.IndexOf(')') - param.IndexOf(',') - 1);
+
+            return new Parameter(name, value, type);
+        }
+    }
+
     class Program
     {
+        static List<string> ReadAllFromFile(char[] charSeparators, string path)
+        {
+            List<string> stringsFromFile = new List<string>();
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File does not exist.");
+                return stringsFromFile;
+            }
+
+            using (FileStream fstream = File.OpenRead(path))
+            {
+                byte[] info = new byte[fstream.Length];
+                fstream.Read(info, 0, info.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(info);
+                stringsFromFile = textFromFile.Split(charSeparators, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+            }
+
+            return stringsFromFile;
+        }
+
+        static void WriteInFile(string path, List<string> writingStrings)
+        {
+            using (FileStream fstream = new FileStream(path, FileMode.Create))
+            {
+                string writingString = "";
+
+                foreach (string str in writingStrings)
+                {
+                    writingString = writingString + str + '\n';
+                }
+                // запись массива байтов в файл
+                byte[] array = System.Text.Encoding.Default.GetBytes(writingString);
+                fstream.Write(array, 0, array.Length);
+            }
+        }
+
+        static (List<Parameter> AddedParams, List<Parameter> DeletedParams) LoadRules(char[] charSeparators, string path)
+        {
+
+            List<Parameter> addParams = new List<Parameter>();
+            List<Parameter> deleteParams = new List<Parameter>();
+            List<string> rules = ReadAllFromFile(charSeparators, path);
+            foreach (string strParam in rules)
+            {
+                Parameter newParameter = strParam;
+                switch (newParameter.ParameterType)
+                {
+                    case Parameter.ParamType.delete:
+                        deleteParams.Add(newParameter);
+                        break;
+                    case Parameter.ParamType.add:
+                        addParams.Add(newParameter);
+                        break;
+                }
+            }
+
+            return (addParams, deleteParams);
+        }
+
         static void Main(string[] args)
         {
+            char[] charSeparators = { '\n' };
+
+            //load new rules
+            string ruleFilePath = "rules.txt";
+            var rules = LoadRules(charSeparators, ruleFilePath);
+
+            if (rules.AddedParams.Count==0 && rules.DeletedParams.Count == 0)
+            {
+                return;
+            }
+
+            string profileManagerFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                @"\Mozilla\Firefox\profiles.ini";
+
+            List<string> stringsFromFile = ReadAllFromFile(charSeparators, profileManagerFile);
+
+            string defaultPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                @"\Mozilla\Firefox\Profiles\";
+
+            IEnumerable<string> profilePaths = from path in stringsFromFile
+                                               where path.IndexOf("Path") > -1
+                                               select path.IndexOf("Path=Profiles") > -1 ?
+                                               path.Replace("Path=Profiles/", defaultPath) :
+                                               path.Replace("Path =", "");
+
+            foreach (string path in profilePaths)
+            {
+                var parameters = ReadAllFromFile(charSeparators,path.TrimEnd(new char[] {'\r'}) + @"\prefs.js");
+
+                foreach (var rule in rules.AddedParams)
+                {
+                    parameters.Insert(parameters.Count,rule.ParamToString() + '\r');
+                }
+
+                foreach (var rule in rules.DeletedParams)
+                {
+                    parameters.Remove(rule.ParamToString() + '\r');
+                }
+
+                WriteInFile(path.TrimEnd(new char[] { '\r' }) + @"\prefs.js",parameters);
+            }
         }
     }
 }
